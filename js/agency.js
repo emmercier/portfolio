80b2067aeba729fa3e4f09fc84aabac0f36b80b3
emmercier <em.mercier@gmx.fr>
2015-03-25 20:31:21 +0100

    Changements
    Modif' de la courbe de bezier, rajout lien contact, modif' du fond de la
    nav collapse, modif' du sous h2 "Qui suis-je", modif' du <header> et
    rajout d'une section "page-top".

--- js/agency.js
+++ js/agency.js
@@ -10,7 +10,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 2500, 'easeOutExpo'); //avant modif : 1500, et easeInOutExpo
         event.preventDefault();
     });
 });
